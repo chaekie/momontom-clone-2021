91a0b71b8d3de7667e398db662bfffc5d5c1bbd8
chaekie <chaekie.kim@gmail.com>
2021-02-17 20:56:11 +0900

    create index

---
+++ index.js
@@ -0,0 +1,41 @@
+/*
+const mood = "😁";
+const date = "2021.02.17";
+const want = "sleep";
+const what = "595959";
+console.log(mood);
+
+const eyesight = 1.2;
+
+const balance = 20000;
+
+const coffee = false;
+
+
+console.log(eyesight);
+console.log(balance);
+console.log(coffee);
+
+
+const a = 221;
+let b = a - 5;
+a = 4;
+console.log(b, a);
+
+alert("Im Working. Im JSON. Im Beautiful and Worth it.");
+console.log("Im Working. Im JSON. Im Beautiful and Worth it.");
+
+
+const monday = "Mom";
+const tue = "Tue";
+const wed = "Wed";
+const thu = "Thu";
+const fri = "Fri";
+
+console.log(monday, tue, wed, thu, fri);
+
+const daysOfWeek = ["mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+// console.log(daysOfWeek);
+
+console.log(daysOfWeek[2]);
+console.log(daysOfWeek[4]);*/
