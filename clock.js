d07234d4cc049dfee9e3eada62b8209712d7ba1f
chaekie <chaekie.kim@gmail.com>
2021-03-05 18:18:50 +0900

    make to do list 1

--- clock.js
+++ clock.js
@@ -1,14 +1,14 @@
-"use strict;";
+"use strict";
 
-const clokcContainer = document.querySelector(".js-clock"),
-  clockTitle = clokcContainer.querySelector("h1");
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
   const date = new Date();
-  const minutes = date.getMinutes();
   const hours = date.getHours();
+  const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `$0{hours}` : hours}:${
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
